1fe95f3ac293bcd77fbb5c0691c297e3be33cc0f
saparbekovtemirlan <122355879@mail.ru>
2022-06-17 23:59:23 +0600

    a

--- task-02/index.js
+++ task-02/index.js
@@ -1,3 +1,9 @@
-function test() {
-    
+function test(num) {
+  let arr = [2, 3, 4, 5];
+  for (let i = 0; i < arr.length; i++){
+      if (num % arr[i] == 0){
+          console.log(arr[i]);
+      }
+  }
 }
+test(55);
